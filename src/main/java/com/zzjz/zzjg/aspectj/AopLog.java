3de01962879f81e28dc60c77ebcb65421d81e357
FGTang <gui66497@hotmail.com>
2019-08-20 13:50:12 +0800

    添加Aop拦截生成日志功能

---
+++ src/main/java/com/zzjz/zzjg/aspectj/AopLog.java
@@ -0,0 +1,93 @@
+package com.zzjz.zzjg.aspectj;
+
+import eu.bitwalker.useragentutils.UserAgent;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import cn.hutool.json.JSONUtil;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @Description Aop拦截生成日志
+ * @Author 房桂堂
+ * @Date 2019/8/20 10:23
+ */
+@Aspect
+@Component
+public class AopLog {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AopLog.class);
+
+    private static final String START_TIME = "request-start";
+
+    /**
+     * 切入点
+     */
+    @Pointcut("execution(public * com.zzjz.zzjg.controller.*Controller.*(..))")
+    public void log() {
+
+    }
+
+    /**
+     * 前置操作
+     *
+     * @param point 切入点
+     */
+    @Before("log()")
+    public void beforeLog(JoinPoint point) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+        HttpServletRequest request = Objects.requireNonNull(attributes).getRequest();
+
+        LOGGER.info("【请求 URL】：{}", request.getRequestURL());
+        LOGGER.info("【请求 IP】：{}", request.getRemoteAddr());
+        LOGGER.info("【请求类名】：{}，【请求方法名】：{}", point.getSignature().getDeclaringTypeName(), point.getSignature().getName());
+
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        LOGGER.info("【请求参数】：{}，", JSONUtil.toJsonStr(parameterMap));
+        Long start = System.currentTimeMillis();
+        request.setAttribute(START_TIME, start);
+    }
+
+    /**
+     * 环绕操作
+     *
+     * @param point 切入点
+     * @return 原方法返回值
+     * @throws Throwable 异常信息
+     */
+    @Around("log()")
+    public Object aroundLog(ProceedingJoinPoint point) throws Throwable {
+        Object result = point.proceed();
+        LOGGER.info("【返回值】：{}", JSONUtil.toJsonStr(result));
+        return result;
+    }
+
+    /**
+     * 后置操作
+     */
+    @AfterReturning("log()")
+    public void afterReturning() {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = Objects.requireNonNull(attributes).getRequest();
+
+        Long start = (Long) request.getAttribute(START_TIME);
+        Long end = System.currentTimeMillis();
+        LOGGER.info("【请求耗时】：{}毫秒", end - start);
+
+        String header = request.getHeader("User-Agent");
+        UserAgent userAgent = UserAgent.parseUserAgentString(header);
+        LOGGER.info("【浏览器类型】：{}，【操作系统】：{}，【原始User-Agent】：{}", userAgent.getBrowser().toString(), userAgent.getOperatingSystem().toString(), header);
+    }
+}
