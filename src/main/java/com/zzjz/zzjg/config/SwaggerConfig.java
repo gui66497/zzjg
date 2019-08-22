f75eb42c754f91bd49905001185613f3c7209502
FGTang <gui66497@hotmail.com>
2019-08-22 09:26:17 +0800

    为swagger每个接口添加权限认证参数

--- src/main/java/com/zzjz/zzjg/config/SwaggerConfig.java
+++ src/main/java/com/zzjz/zzjg/config/SwaggerConfig.java
@@ -2,14 +2,19 @@ package com.zzjz.zzjg.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * @ClassName: SwaggerConfig
@@ -20,13 +25,26 @@ import java.util.Collections;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    /**
+     * header中的ticket参数非必填，传空也可以
+     * @return api
+     */
     @Bean
     public Docket api() {
+        ParameterBuilder ticketPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<>();
+        ticketPar.name("Authorization").description("令牌")
+                .modelRef(new ModelRef("string")).parameterType("header")
+                .required(false).build();
+        pars.add(ticketPar.build());
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
+                .globalOperationParameters(pars)
                 .apiInfo(apiInfo());
     }
     private ApiInfo apiInfo() {
